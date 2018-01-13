7b6c6760517c116ed0eb92df52d2aa34a32a564b
andydexter <andydexter123@gmail.com>
2018-01-13 12:33:11 +0200

    added poke button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,6 +13,10 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+int presses=0;
+int sizecounter=10;
+//QSize size=10;
+
 void MainWindow::on_checkBox_clicked(bool checked)
 {
     if(checked==true){
@@ -20,5 +24,26 @@ void MainWindow::on_checkBox_clicked(bool checked)
     }
     else if(checked==false){
         ui->label->setText("not tickedddddd!");
+    }
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    presses++;
+//    ui->label->size(size);
+    if(presses==1){
+        ui->label->setText("why you poke");
+    }
+    if(presses>2 && presses<5){
+        ui->label->setText("stop");
+    }
+    if(presses>=5 && presses<8){
+        ui->label->setText("this is not funny");
+    }
+    if(presses>=8 && presses<12){
+        ui->label->setText("this is annoying");
+    }
+    if(presses>=12){
+        ui->label->setText("£#@&^*!:@??/!!!");
     }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,6 +18,8 @@ public:
 private slots:
     void on_checkBox_clicked(bool checked);
 
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
